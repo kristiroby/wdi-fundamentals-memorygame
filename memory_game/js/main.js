38c9f7c5cfbbd7264a86cc756800ebaed465ce74
kristiroby@gmail.com <kristiroby@gmail.com>
2019-05-23 18:36:07 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,24 +1,24 @@
 const cards = ["queen", "queen", "king", "king"];
-
 let cardsInPlay = [];
 
-let cardOne = cards[1];
-
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
-
-let cardTwo = cards[0];
-
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
-
+var checkForMatch = function () {
 if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
 	alert("You found a match!");
 } 
 else if (cardsInPlay.length === 2 && cardsInPlay[0] !== cardsInPlay[1]) {
 	alert("Sorry try again");
+}	
 }
 
-	
+function flipCard (cardId){
+console.log("User flipped " + cards[cardId]);
+cardsInPlay.push(cards[cardId]);
+
+checkForMatch();
+}
+
+flipCard(0);
+flipCard(1);
+
 
 
